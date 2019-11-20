d454118fa8a39664558e58864e3eacd02af9bb6d
garnu321 <marcin.garnuszek@lo1.sandomierz.pl>
2019-11-20 11:41:11 +0100

    pętla while

---
+++ python/zad2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main(args):
+    start = int(input("Podaj liczbę: "))
+    stop = int(input("Podaj liczbę: "))
+    for i in range(start, stop+1)
+        print(i)
+    return 0
+    
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
