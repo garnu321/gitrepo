937c47a7ba053c08838fffdf3bd628b563c8cdd7
garnu321 <marcin.garnuszek@lo1.sandomierz.pl>
2019-12-12 12:36:32 +0100

    zadanie

--- C++/placa.cpp
+++ C++/placa.cpp
@@ -10,18 +10,20 @@ using namespace std;
 int main(int argc, char **argv)
 {
     float a, b, x;
+    x = 0
     cout << "Podaj liczbę przepracowanych nadgodzin: " << endl;
     cin >> a;
     cout << "Podaj stawkę za jedną nadgodzinę: " << endl;
     cin >> b;
     if (a <= 30) {
-        a * b = x
+        a * b = x; }
         cout << x;
-}   else  {
-        30 * b + (a - 30 * 1,5 * b) = x
+    else   {
+        
+     }
         cout << x; 
 
-}
+
 
     
 	
---
+++ python/zliczanie.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+
+
+def main(args):
+    b = 0
+    c = 0
+    a = int(input("Podaj liczbę: ")
+    while a > 0:
+        if a % 5 == 0: 
+            b = b + 1
+        if a % 7 == 0: 
+            c = b + 1   
+    
+    return 0
+    
+       
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
