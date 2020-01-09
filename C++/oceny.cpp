9500b0eafa58b1f188c10a3d67a9fc2a05c61e4b
garnu321 <marcin.garnuszek@lo1.sandomierz.pl>
2020-01-09 12:37:36 +0100

    Oceny w c++

--- C++/oceny.cpp
+++ C++/oceny.cpp
@@ -8,20 +8,28 @@
 using namespace std;
 int main(int argc, char **argv)
 {
-    float ocena, oceny
-    cout << "Podaj ocenę: ";
-    cin >> ocena;
-    while (ocena > 0) {
-        if (ocena < 7) {
+ float ocena, oceny, suma, srednia;
+    suma = 0;
+    cout << "Podaj liczbe ocen:";
+    cin >> oceny;
+    while (oceny < 1) {
+        cout << "Podaj liczbę ocen:";
+        cin >> oceny;
+    }
+    for(int i = 0; i < oceny; i = i + 1) {
+        cout << "Podaj ocenę:";
+        cin >> ocena;
+        while (ocena < 1 or ocena > 6) {
+            cout << "Błędna ocena! Podaj poprawną: ";
+            cin >> ocena;
             
         }
-    else {
-        cout << "Błędna ocena!";
-    }    
-    cout << "Podaj ocenę: ";
-    cin >> ocena;
+        suma = suma + ocena;
     }
-	
+    srednia = suma / oceny;
+    cout << "Twoja średnia wynosi: ";
+    cout << srednia;
+
 	return 0;
 }
 
