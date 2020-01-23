4ad142be515baf795706b354f3d0162782850dff
garnu321 <marcin.garnuszek@lo1.sandomierz.pl>
2020-01-23 12:36:12 +0100

    kalkulator w pythonie

--- python/kalkulator.py
+++ python/kalkulator.py
@@ -5,12 +5,48 @@
 
 wynik = 0
 
-def pobierzLiczbe()
-    x = int(input("Podaj pierwszą liczbę: "))
-    y = int(input("Podaj drugą liczbę: "))
-
+def pobierzLiczbe():
+    x = int(input("Podaj liczbę: "))
+    return x
+    
+def dodaj(x1, x2):
+    global wynik
+    wynik = x1 + x2 
+    
+    
+def odejmij(x1, x2):
+    global wynik
+    wynik = x1 - x2
+   
+ 
+def pomnoz(x1, x2):
+    global
+    wynik = x1 * x2
+    
+   
+def podziel(x1, x2):
+    wynik = x1 / x2             
+       
+    
+def drukuj():
+    print(wynik)
+    
 def main(args):
-    operacja = input("Jakie działanie chcesz wykonać?(*, /, +, -)")
+    operacja = input("Jakie działanie chcesz wykonać?(*, /, +, -): ")
+    x1 = pobierzLiczbe()
+    x2 = pobierzLiczbe()    
+    if operacja == "+":
+        dodaj(x1, x2)
+    elif operacja == "-":
+        odejmij(x1, x2) 
+    elif operacja == "*":
+        pomnoz(x1, x2)
+    elif operacja == "/":
+        podziel(x1, x2)                 
+    else:
+        print("Błedny znak działania!") 
+    drukuj()      
+        
     return 0
 
 if __name__ == '__main__':
